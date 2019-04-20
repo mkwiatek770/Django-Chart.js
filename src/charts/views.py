6bb628f95c8ead4a4d6f78b64839cd33a4b6f8df
Michał Kwiatek <michalkwiatek9@o2.pl>
2019-04-20 18:20:25 +0200

    Displaying charts using chart.js in Django Project

--- src/charts/views.py
+++ src/charts/views.py
@@ -1,26 +1,23 @@
-from django.contrib.auth import get_user_model
-from django.http import JsonResponse
+from django.views import View
 from django.shortcuts import render
-from django.views.generic import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-User = get_user_model()
-
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'charts.html', {"customers": 10})
-
+        return render(request, 'charts.html', {})
 
 
 def get_data(request, *args, **kwargs):
     data = {
         "sales": 100,
-        "customers": 10,
+        "customers": 10
     }
-    return JsonResponse(data) # http response
+    return JsonResponse(data)
 
 
 class ChartData(APIView):
@@ -28,12 +25,16 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
-        qs_count = User.objects.all().count()
-        labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
-        default_items = [qs_count, 23, 2, 3, 12, 2]
+        labels = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+        default_items = [User.objects.all().count(), 2, 12, 2, 9, 1]
         data = {
-                "labels": labels,
-                "default": default_items,
+            'labels': labels,
+            "default": default_items
         }
         return Response(data)
 
+
+# 3 sposoby na wysyłanie danych do frontendu
+# najlepszym sposobem jest użycie DRF
+# DRF daje nam funkcjonalności taki jak authentication oraz permission
+# używając DRF również możemy przesyłać querysety np. User.objects.all()
